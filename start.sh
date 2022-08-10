9b65b83ff8b1df6d1cf3a9448b7b1edf6c4c9f0d
Lucifer Morningstar <103210935+Lucifer86790@users.noreply.github.com>
2022-08-10 22:45:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Ps705112/potter-by-me.git /potter-by-me
+  git clone https://github.com/Lucifer86790/Movie-file-bot.git /Movie-file-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /potter-by-me
+  git clone $UPSTREAM_REPO /Movie-file-bot
 fi
-cd /potter-by-me
+cd /Movie-file-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
