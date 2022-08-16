f3e64128a8efe6c399205ab34e9405176ae79db4
Lucifer Morningstar <103210935+Lucifer86790@users.noreply.github.com>
2022-08-16 21:01:03 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -252,7 +252,7 @@ async def start(client, message):
             f_caption=f_caption
     if f_caption is None:
         f_caption = f"{files.file_name}"
-        ravi1 = await client.send_cached_media(
+    ravi1 = await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
