7cc99e62c12f8ab29b9db92f5486148070463474
Ravinandan <104293257+RaviTechnoMindz@users.noreply.github.com>
2022-08-29 19:24:27 +0530

    removed unwanted

--- plugins/commands.py
+++ plugins/commands.py
@@ -56,10 +56,6 @@ async def start(client, message):
             InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
         ]]    
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_chat_action("Typing")
-        m=await message.reply_sticker("CAACAgUAAxkBAAEFXjpi3UkE0ZhynSwLX09m3hbV8mT3_QACagQAAuW0-FcThm6lMMZRtikE")
-        await asyncio.sleep(1)
-        await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
