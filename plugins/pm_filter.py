c0aa9d9e7e95a77ef0f7ee565de712eecceb1d9e
Ravinandan <104293257+RaviTechnoMindz@users.noreply.github.com>
2022-08-29 19:23:44 +0530

    removed unwanted

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -433,10 +433,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
       
         ]]    
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_chat_action("Typing")
-        m=await message.reply_sticker("CAACAgUAAxkBAAEFXjpi3UkE0ZhynSwLX09m3hbV8mT3_QACagQAAuW0-FcThm6lMMZRtikE") 
-        await asyncio.sleep(1)
-        await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
